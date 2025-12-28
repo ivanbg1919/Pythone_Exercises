0f876f92b89c523e7b89289635836d68dcccb396
Ivan <74875827+ivanbg1919@users.noreply.github.com>
2025-12-29 00:01:27 +0100

    Add files via upload

--- birthdays.py
+++ birthdays.py
@@ -3,14 +3,14 @@
 birthdays = {'Mica':'26.1', 'Igor': '26.7', 'Fiki': '15.9', 'Koka': '6.12'}
 
 while True:
-    print("Please enter a name: (blank to quit)")
+    print("Please enter a name: (blank to quit) ")
     name = input()
     if name == '':
         break
     if name in birthdays:
-        print(f"{birthdays[name]} is birthday of {name}")
+        print(f"{birthdays[name]} is the birthday of {name}")
     else:
-        print(f"I don't have information {name}")
+        print(f"I don't have birthday information for {name}")
         print("What is their birthday?")
         bday = input()
         birthdays[name] = bday
